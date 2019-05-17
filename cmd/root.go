6fa90a2d0126fbb6a5e1ee58e3770cd0f5bff290
david <david.genest@gmail.com>
2019-05-17 00:20:12 -0400

    output version info in json format

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 
 	"github.com/gobuffalo/packr/v2"
 	"github.com/spf13/cobra"
@@ -28,6 +30,7 @@ type mainCmd struct {
 // CreateRoot creates the root command
 func createRootCmd(driver summon.Interface) *cobra.Command {
 	cmdName := filepath.Base(os.Args[0])
+	var showVersion bool
 
 	main := &mainCmd{
 		driver: driver,
@@ -37,7 +40,7 @@ func createRootCmd(driver summon.Interface) *cobra.Command {
 		Use:   cmdName + " [file to summon]",
 		Short: cmdName + " main command",
 		Args: func(cmd *cobra.Command, args []string) error {
-			if main.copyAll {
+			if main.copyAll || showVersion {
 				return nil
 			}
 			if len(args) < 1 {
@@ -50,6 +53,17 @@ func createRootCmd(driver summon.Interface) *cobra.Command {
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			main.out = cmd.OutOrStdout()
+			if showVersion {
+				v, ok := makeVersion()
+				if !ok {
+					fmt.Fprintln(cmd.OutOrStderr(), "Missing build info")
+					return nil
+				}
+				enc := json.NewEncoder(main.out)
+				enc.SetIndent("", "  ")
+				enc.Encode(v)
+				return nil
+			}
 			if !main.copyAll {
 				filename := args[0]
 				main.filename = filename
@@ -78,6 +92,7 @@ func createRootCmd(driver summon.Interface) *cobra.Command {
 	rootCmd.Flags().BoolVarP(&main.copyAll, "all", "a", false, "restitute all data")
 	rootCmd.Flags().BoolVar(&main.raw, "raw", false, "output without any template rendering")
 	rootCmd.Flags().StringVarP(&main.dest, "out", "o", config.OutputDir, "destination directory, or '-' for stdout")
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "output data version info and exit")
 
 	rootCmd.AddCommand(newListCmd(driver))
 	rootCmd.AddCommand(newRunCmd(driver))
@@ -110,4 +125,37 @@ func Execute(box *packr.Box) error {
 	}
 	rootCmd := createRootCmd(s)
 	return rootCmd.Execute()
+}
+
+type versionDesc struct {
+	Exe     string `json:"exe,omitempty"`
+	Mod     string `json:"mod"`
+	Version string `json:"version"`
+}
+type versionInfo struct {
+	Assets versionDesc `json:"assets"`
+	Lib    versionDesc `json:"lib"`
+}
+
+func makeVersion() (v versionInfo, ok bool) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return v, false
+	}
+	v = versionInfo{
+		Lib: versionDesc{},
+		Assets: versionDesc{
+			Mod:     bi.Main.Path,
+			Version: bi.Main.Version,
+			Exe:     os.Args[0],
+		},
+	}
+	for _, d := range bi.Deps {
+		if d.Path == "github.com/davidovich/summon" {
+			v.Lib.Mod = d.Path
+			v.Lib.Version = d.Version
+			break
+		}
+	}
+	return v, true
 }
