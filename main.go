317d8e85b64b287acff9f37230cdb91804f5b993
david <david.genest@gmail.com>
2019-09-11 22:29:40 -0400

    better doc for intro and Main() function

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 /*
-Package summon is a library allowing to pack assets and give it a convienient command line.
+Summon is a library that allows giving a convienient command line to packed assets.
 
 The binary created from this library is meant to be shared in a team to allow distribution
 of common assets or code templates.
 
+It solves the maintenance problem of multiple copies of same
+code snippets distributed in many repos (like general makefile recipies), leveraging go modules and version
+management. It also allows configuring a standard set of tools that a dev team can readily
+invoke by name.
+
 Basics
 
 This library needs a command entrypoint in a data repository. See https://github.com/davidovich/summon-example-assets.
+
 */
 package summon
 
@@ -21,8 +27,9 @@ import (
 )
 
 // Main entrypoint, typically called from a data repository. Calling Main() relinquishes
-// control to Summon so it can manage the command line arguments and instanciation of assets in
-// the packr.Box data repository.
+// control to Summon so it can manage the command line arguments and instanciation of assets
+// located in the passed packr.Box data repository.
+// See https://github.com/gobuffalo/packr/tree/master/v2 for more information on packr Boxes.
 func Main(args []string, box *packr.Box) int {
 	s, err := summon.New(box)
 	if err != nil {
