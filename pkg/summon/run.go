1426ba68b23c19c240ce91b618a3520f5730a678
david <david.genest@gmail.com>
2022-01-26 20:29:36 -0500

    Support --help on root cmd (no args)

--- pkg/summon/run.go
+++ pkg/summon/run.go
@@ -433,11 +433,13 @@ func (d *Driver) setupArgs(root *cobra.Command) {
 		managedHelp = append(managedHelp, a)
 	}
 
-	// if help is requested on a managed command, let cobra manage the help flag
+	// if help is requested on:
+	//   * a managed command that has a help line
+	//   * on the root (no parameters)
 	var ownHelp bool
 	if helpFlag != "" {
 		cmd, _, _ := root.Root().Find(allArgs[:helpPos])
-		if cmd != root && cmd.Short != "" {
+		if cmd != root && cmd.Short != "" || len(managedHelp) == 0 {
 			ownHelp = true
 		}
 	}
