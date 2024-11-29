270e6ca2337b53b827f37965f6a30e97f35d8a1c
davidovich <david.genest@gmail.com>
2024-11-28 22:06:12 -0500

    extracts templating to own file

--- pkg/summon/summon.go
+++ pkg/summon/summon.go
@@ -13,11 +13,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"reflect"
-	"strings"
-	"text/template"
 
-	"github.com/Masterminds/sprig/v3"
 	"github.com/spf13/afero"
 
 	"github.com/davidovich/summon/internal/testutil"
@@ -101,143 +97,12 @@ func makeCopyFileFun(startdir string, d *Driver) func(path string, de fs.DirEntr
 	}
 }
 
-func (d *Driver) prepareTemplate() (*template.Template, error) {
-	t := d.templateCtx
-	var err error
-	if t != nil {
-		t, err = t.Clone()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		t = template.New(Name)
-	}
-
-	t.Option("missingkey=zero").
-		Funcs(sprig.TxtFuncMap()).
-		Funcs(summonFuncMap(d))
-
-	return t, nil
-}
-
-func executeTemplate(t *template.Template, data interface{}) (string, error) {
-	buf := &bytes.Buffer{}
-	err := t.Execute(buf, data)
-
-	// The zero value for an interface is a nil interface{} which
-	// has a string representation of <no value>. Strip this out.
-	// https://github.com/golang/go/issues/24963
-	return strings.ReplaceAll(buf.String(), "<no value>", ""), err
-}
-
-func (d *Driver) renderTemplate(tmpl string) (string, error) {
-	t, err := d.prepareTemplate()
-	if err != nil {
-		return tmpl, err
-	}
-
-	t, err = t.Parse(tmpl)
-	if err != nil {
-		return tmpl, err
-	}
-
-	data := d.opts.data
-	return executeTemplate(t, data)
-}
-
 func (d *Driver) resolveAlias(alias string) string {
 	if resolved, ok := d.config.Aliases[alias]; ok {
 		return resolved
 	}
 
 	return alias
-}
-
-func summonFuncMap(d *Driver) template.FuncMap {
-	initConsumed := func() {
-		if d.opts.argsConsumed == nil {
-			d.opts.argsConsumed = make(map[int]struct{}, len(d.opts.args))
-		}
-	}
-	consumeAllArgs := func() {
-		initConsumed()
-		for i := range d.opts.args {
-			d.opts.argsConsumed[i] = struct{}{}
-		}
-	}
-	return template.FuncMap{
-		"run": func(args ...string) (string, error) {
-			driverCopy := Driver{
-				opts:        d.opts,
-				config:      d.config,
-				fs:          d.fs,
-				baseDataDir: d.baseDataDir,
-				templateCtx: d.templateCtx,
-				execCommand: d.execCommand,
-				configRead:  d.configRead,
-				cmdToSpec:   d.cmdToSpec,
-			}
-			driverCopy.opts.argsConsumed = map[int]struct{}{}
-			driverCopy.opts.cobraCmd = nil
-			driverCopy.opts.helpWanted.helpFlag = ""
-
-			b := &strings.Builder{}
-			err := driverCopy.Run(Ref(args[0]), Args(args[1:]...), Out(b))
-
-			if d.opts.dryrun {
-				b.WriteString("[")
-				b.WriteString(args[0])
-				b.WriteString(" (dry-run)]")
-			}
-
-			if d.opts.debug {
-				fmt.Fprintf(os.Stderr, "Output [%s] -> `%s`...\n", args[0], b)
-			}
-			return strings.TrimSpace(b.String()), err
-		},
-		"summon": func(path string, arg ...any) (string, error) {
-			dest := os.TempDir()
-			if len(arg) > 0 {
-				if reflect.TypeOf(arg[0]).Kind() == reflect.String {
-					dest = arg[0].(string)
-				}
-			}
-			return d.Summon(Filename(path), Dest(dest))
-		},
-		"flagValue": func(flag string) (string, error) {
-			for _, toRender := range d.flagsToRender {
-				if toRender.name == flag {
-					toRender.explicit = true
-					return toRender.renderTemplate()
-				}
-			}
-			return "", nil
-		},
-		"arg": func(index int, missingErrors ...string) (string, error) {
-			missingError := strings.Join(missingErrors, " ")
-			if d.opts.args == nil {
-				return "", fmt.Errorf(missingError)
-			}
-			if index >= len(d.opts.args) {
-				return "", fmt.Errorf("%s: index %v out of range, args: %s", missingError, index, d.opts.args)
-			}
-
-			retrieved := d.opts.args[index]
-			initConsumed()
-			d.opts.argsConsumed[index] = struct{}{}
-			return retrieved, nil
-		},
-		"args": func() []string {
-			consumeAllArgs()
-
-			return d.opts.args
-		},
-		"swallowargs": func() string {
-			consumeAllArgs()
-
-			return ""
-		},
-	}
 }
 
 func (d *Driver) copyOneFile(embeddedFile fs.File, filename, root string) (string, error) {
